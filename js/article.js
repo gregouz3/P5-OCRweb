f34afaa33b80f2c0b12008f695b23c0eea3e7fc9
Gregoire De Fleurac <45977768+gregouz3@users.noreply.github.com>
2020-06-26 04:03:07 +0200

    v1

--- js/article.js
+++ js/article.js
@@ -1,65 +1,56 @@
-function get_prod()  {
+function onLoadCartNumbers() {
 
-  return new Promise((response) =>{
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-
-      if (this.readyState == XMLHttpRequest.DONE && this.status == 200) 
-        response(JSON.parse(this.responseText));
-    }
-    request.open("GET", "http://localhost:3000/api/furniture/" + location.search.substring(4));
-    request.send();
-  });
+  let prodNumbers = localStorage.getItem('cartNumbers');
+  if (prodNumbers) 
+    document.querySelector('.num').textContent = prodNumbers;
 }
 
-async function show_prod() {
-  
+function show_prod() {
+
   onLoadCartNumbers();
-  let produ = document.getElementById('product');
-  const pro = await get_prod();
   
-    produ.innerHTML += `
-      <div class="row mt-2">
-        <img   class="img-fluid  col-md-8 block-prod__img mb-1" src="${pro.imageUrl}" alt="image article">
-        <div class="col-md-4 d-flex flex-column justify-content-center align-items-center">
-        <p class="block-prod__name">${pro.name} </p>
-        <div class="form-group">
+ 
+  let produ = document.getElementById('product');
+  let pro = localStorage.getItem("prodView");
+  pro = JSON.parse(pro);
+  
+  produ.innerHTML += `
+    <div class="row mt-2">
+      <img   class="img-fluid  col-md-8 block-prod__img mb-1" src="${pro.imgSrc}" alt="image article">
+      <div class="col-md-4 d-flex flex-column justify-content-center align-items-center">
+        <p class="block-prod__name ">${pro.name} </p>
+        <div class="form-group ">
           <label for="vernis"></label>
-          <select name="verni" id="vernis"></select>
+          <select class="form-control m-auto" id="vernis"></select>
         </div>
       </div>
-      </div>
-      <p class="block-prod__tag mt-3">${pro.description} </p>
-      <p class="block-prod__price d-flex justify-content-center ">${pro.price / 100} &euro;</p>
-      <div class="row d-flex justify-content-around mb-2">
-        <a href="index.html" title="go back"><ion-icon class="block-prod__icon"name="arrow-back-outline"></ion-icon></a>
-        <button  type="button" class="btn btn-light block-prod__btn">Add to Cart</button>
-        <a href="panier.html" title="view my cart"><ion-icon class="block-prod__icon" name="cart-outline"></ion-icon></a>
-      </div>
-    `;
-  for (verni of pro.varnish) {
+    </div>
+    <p class="block-prod__tag mt-3">${pro.description} </p>
+    <p class="block-prod__price d-flex justify-content-center ">${pro.price} &euro;</p>
+    <div class="row d-flex justify-content-around mb-2">
+      <a href="index.html" title="go back"><ion-icon class="block-prod__icon"name="arrow-back-outline"></ion-icon></a>
+      <button  type="button" class="btn btn-light block-prod__btn">Add to Cart</button>
+      <a href="panier.html" title="view my cart"><ion-icon class="block-prod__icon" name="cart-outline"></ion-icon></a>
+    </div>
+  </div>
+  `;
+  for (verni of pro.option) {
     let vernis = document.getElementById("vernis");
     let vrnsh = document.createElement("option");
     vernis.appendChild(vrnsh).innerHTML = verni;
   }
-  let products = [
-    name = pro.name,
-    description = pro.description,
-    price = pro.price / 100,
-    idMeuble = pro._id,
-    imgSrc = pro.imageUrl,
-  ];
   let add_cart = document.querySelectorAll('.block-prod__btn');
   for (let i=0; i < add_cart.length; i++) {
     add_cart[i].addEventListener('click', () => {
-        prod_inCart(products);
-        totalPrice(products);
-   
+      prod_inCart(pro);
+      prod_Qt(pro);
+      totalPrice(pro);
     })
   }
 }
-function prod_inCart(products) {
 
+function prod_inCart(products) {
+ 
   let countCart = document.querySelector('.num');
   let prodNumbers = localStorage.getItem('cartNumbers');
   prodNumbers = parseInt(prodNumbers);
@@ -72,49 +63,68 @@ function prod_inCart(products) {
     countCart.textContent = 1;
   }
   setItems(products);
-
-
-}
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
 }
 
 function setItems(products) {
 
-  let prodNumbers = localStorage.getItem('cartNumbers')
   let cartItems = localStorage.getItem('prodInCart');
   cartItems = JSON.parse(cartItems);
   if (cartItems != null) {
 
-          cartItems = {
-            ...cartItems,
-            [products[3]]: products
-          }
-      }
-  else {
+    if (cartItems[products.name] == undefined) {
       cartItems = {
-        [products[3]]: products
+        ...cartItems,
+        [products.name]: products
       }
-
+    }
+  } 
+  else {
+    cartItems = {
+      [products.name]: products
+    }
   }
   localStorage.setItem("prodInCart", JSON.stringify(cartItems));
-
 }
 
+function prod_Qt(products) {
+  
+  let cartQt = localStorage.getItem('prod_Qt');
+  cartQt = JSON.parse(cartQt);
+  let quantity = 0;
+  if (cartQt != null) {
+    console.log("pasnul");
+
+    if (cartQt[products.idMeuble] == undefined) {
+      cartQt = {
+        ...cartQt,
+        [products.idMeuble]: products
+      }
+    }
+    if (cartQt[products.idMeuble].quantity != null)
+      cartQt[products.idMeuble].quantity += 1;
+    products.quantity = 1;
+    cartQt[products.idMeuble].total = parseInt(cartQt[products.idMeuble].quantity) *parseInt(cartQt[products.idMeuble].price)  ;
+    
+
+  }
+  else {
+    products.quantity = 1;
+    cartQt = {
+      [products.idMeuble]: products
+    }
+    products.total = parseInt(products.price);
+
+
+  }
+  localStorage.setItem("prod_Qt", JSON.stringify(cartQt));
+}
 
 function totalPrice(products) {
   let tt = localStorage.getItem('totalPrice');
   if (tt != null ) 
-    localStorage.setItem("totalPrice", parseInt(tt) + products[2]);
+    localStorage.setItem("totalPrice", parseInt(tt) + products.price);
   else 
-    localStorage.setItem("totalPrice", products[2]);
-}
-
-function onLoadCartNumbers() {
-
-  let prodNumbers = localStorage.getItem('cartNumbers');
-  if (prodNumbers) 
-    document.querySelector('.num').textContent = prodNumbers;
+    localStorage.setItem("totalPrice", products.price);
 }
 
 show_prod();
